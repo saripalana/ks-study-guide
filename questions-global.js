799b52725e8fe8db5f539a7af707ad0e4689524f
saripalana <saripalana@gmail.com>
2026-07-19 02:25:10 -0400

    Activate bank registry after question data loads

--- questions-global.js
+++ questions-global.js
@@ -1,1 +1,3 @@
 window.QUESTIONS = QUESTIONS;
+if (typeof window.BoardsBootstrapQuestionBanks !== 'function') throw new Error('Question-bank bootstrap is unavailable.');
+window.BoardsBootstrapQuestionBanks(window.QUESTIONS);
